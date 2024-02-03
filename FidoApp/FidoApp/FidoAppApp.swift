908732db0a8333c3eaad4b68ed06bf4b02eedcb1
Ana Neto <ana.neto@impossible.com>
2024-02-03 15:26:38 +0000

    add basic SwiftUI project structure.

---
+++ FidoApp/FidoApp/FidoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidoAppApp.swift
+//  FidoApp
+//
+//  Created by Ana Neto on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FidoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
